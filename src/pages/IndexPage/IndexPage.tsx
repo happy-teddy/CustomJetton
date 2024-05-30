c127c3af9c84e3f991590df14e35608c66645e34
happy-teddy <hidev537@gmail.com>
2024-05-30 16:08:18 -0700

    fix: eslint

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,18 +1,17 @@
-import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
+import { TonConnectButton } from "@tonconnect/ui-react";
 import { Button } from "@telegram-apps/telegram-ui";
 import type { FC } from "react";
 import { useState } from "react";
 
 import "./IndexPage.css";
 import { useJettonContract } from "@/hooks/useJettonContract";
-import { useUtils } from "@tma.js/sdk-react";
 import { useTonConnect } from "@/hooks/useTonConnect";
 
 export const IndexPage: FC = () => {
   const [mintAmount, setMintAmount] = useState(1);
-  const wallet = useTonWallet();
+  // const wallet = useTonWallet();
   const { connected } = useTonConnect();
-  const utils = useUtils(true);
+  // const utils = useUtils(true);
   const { mint } = useJettonContract();
 
   // if (!wallet) {
